c03cefd7674d6f8cfefa10bb6cf8456fd4a48c74
Napuu <santeri.kaariainen@iki.fi>
2021-11-23 21:50:07 +0200

    listen everything

--- listener.py
+++ listener.py
@@ -4,22 +4,19 @@ import time
 import json
 from signal import signal, SIGINT
 
-mmsi_map = {
-    "276813000": "xprs",
-    "276672000": "star",
-    "276829000": "megastar",
-}
+message_count = 0
+
+buffer = ""
 
 def on_message(client, userdata, message):
     str_message = str(message.payload.decode('utf-8'))
     parsed_message = json.loads(str_message)
     mmsi = parsed_message["properties"]["mmsi"]
     ts = parsed_message["properties"]["timestampExternal"]
-    if str(mmsi) in mmsi_map:
-        ship_short_name = mmsi_map[str(mmsi)]
-        print("received message with mmsi", mmsi, ts)
-        with open(f'vessel_locations_{ship_short_name}_{mmsi}', "a") as f:
-            f.write(json.dumps(parsed_message) + "\n")
+    global message_count
+    global buffer
+    message_count += 1
+    buffer += json.dumps(parsed_message) + "\n"
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -50,5 +47,10 @@ if __name__ == '__main__':
     client.loop_start()
     print('Running. Press CTRL-C to exit.')
     while True:
-        time.sleep(0.2)
+        time.sleep(60)
+        print(f'current rate: {message_count}/60s')
+        with open(f'digitraffic_ais_dump', "a") as f:
+            f.write(buffer)
+            buffer = ""
+        message_count = 0
         pass
